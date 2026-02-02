2466c2864c327fe1ff454961cb3351c955342e30
Shakthyoffl <160217199+Shakthyoffl@users.noreply.github.com>
2026-02-02 10:46:04 +0530

    Update sticker ID and tutorial link in commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,7 +30,7 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     try:
-        stick_id = "CAACAgUAAxkBAAEQJmJpViid_0yscWKPfh3RMCY8pIkmXwACMAcAAqzbsFexyKU6FPQAAjgE"
+        stick_id = "CAACAgIAAxkBAAIvYmmAL9aT9RSGe-NOhuCMmrHFWekIAALOFwAC6Y7gSUi1pSJ_Okp9HgQ"
         try:
             sticker = await message.reply_sticker(sticker=stick_id)
         except Exception as e:
@@ -1071,7 +1071,7 @@ async def set_tutorial(client, message: Message):
     except IndexError:
         return await message.reply_text(
             f"<b>ᴄᴏᴍᴍᴀɴᴅ ɪɴᴄᴏᴍᴘʟᴇᴛᴇ !!\n\nᴜꜱᴇ ʟɪᴋᴇ ᴛʜɪꜱ -</b>\n\n"
-            f"<code>/{message.command[0]} https://t.me/dreamxbotz</code>"
+            f"<code>/{message.command[0]} https://t.me/Get1TamilMoviez</code>"
         )
     if message.command[0] == "set_tutorial":
         tutorial_key = "tutorial"
