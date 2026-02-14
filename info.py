f38ece068e7543b0f6f1ff53f7b1d180f3ad7a1c
Shakthyoffl <160217199+Shakthyoffl@users.noreply.github.com>
2026-02-14 23:38:18 +0530

    Update BOT_TOKEN in info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'LeoX')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '27549502')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', '35892287036e27af8e9298f03483fdd4')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "7735629958:AAEGyrV4hU5_oY4PVei2HuPCDsmF8oOWWPA")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "7735629958:AAHqjdpfTbWgaOWeaFaas8o5WB2O9XfQFcg")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
